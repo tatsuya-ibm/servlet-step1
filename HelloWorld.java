e5fc6bb1eabc8ff8890edf155a6f78b751f4cc5b
Tatsuya Miyazaki <116046721+tatsuya-ibm@users.noreply.github.com>
2023-12-21 09:21:28 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Hello World!" + sum);
     }
 }
